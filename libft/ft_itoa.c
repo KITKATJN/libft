cb006967135776497c693dbd0dba7afb11e950f6
Ilya <lvutinilyha@gmail.com>
2020-11-06 00:22:58 +0300

    done main part

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -1,5 +1,35 @@
 #include "libft.h"
 
+static int	ft_len(int n)
+{
+	size_t	len;
+
+	len = 0;
+	if (n == 0)
+		return(1);
+	while (n != 0)
+	{
+		n = n / 10;
+		len++;
+	}
+	return (len);
+}
+
+static char	*Toss_a_coin_to_the_witcher(int n)
+{
+	size_t	len;
+	char	*str;
+	size_t	lutic;
+	
+	len = ft_len(n);
+	lutic = 1;
+	if (n < 0)
+		lutic++;
+	str = malloc(len + lutic);
+	return (str);
+
+}
+
 char	*ft_itoa(int n)
 {
 	size_t	i;
@@ -8,29 +38,21 @@ char	*ft_itoa(int n)
 	long	j;
 
 	j = n;
-	len = 0;
-	while (n != 0)
-	{
-		n = n / 10;
-		len++;
-	}
-	str = malloc(len);
+	len = ft_len(n);
+	str = Toss_a_coin_to_the_witcher(n);
 	if (str)
 	{	
 		i = 0;
 		while (j != 0)
 		{
 			if (j < 0)
-			{
-				str[i] = '-';
-				i++;
-			}
-			str[i] = j / (10 * (len - 1));
-			i++;
+				str[i++] = '-';
+			str[i++] = j / (10 * (len - 1));
+			j = j % (10 * (len - 1));
+			len--;
 		}
 		str[i] = '\0';
 		return (str);
 	}
 	return (0);
-
 }
--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -1,89 +1,42 @@
 #include "libft.h"
 
-static char	*ft_strchr1(const char *s, int c)
+static int	check_set(const char *set, int c)
 {
-	int		i;
-	
-	i = 0;
-	if (c == '\0')
-	{
-		while (s[i] != '\0')
-		{
-			i++;
-		}
-		return ((char*)(s + i));
-	}
-	return (ft_memchr(s,c, ft_strlen((char*)s)));
-}
-
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	char	*str;
-	size_t	i;
-	size_t	right;
-	size_t	left;
-
-	left = 0;
-	if(!s1)
-		return (0);
-	while (s1[left] && ft_strchr1(set, s1[left]))
-		left++;
-	right = ft_strlen(s1) - 1;
-	while (right > left && ft_strchr1(set, s1[right]))
-		right--;
-	str = malloc(right - left + 1);
-	if (!str)
-		return (0);
-	i = 0;
-	while (left < right)
-	{
-		str[i] = s1[left];
-		i++;
-		left++;
-	}
-	str[i] = '\0';
-	return (str);
-}
-
-/*
-static int	check(const char *set, char c)
-{
-	int i;
-	int j;
+	size_t i;
 
 	i = 0;
-	j = 0;
 	while (set[i] != '\0')
 	{
 		if (set[i] == c)
-			j = 1;
+			return (1);
 		i++;
-	}
-	return (j);
-}
-
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	char	*str;
-	size_t	i;
-	size_t	j;
-	size_t 	len;
-
-	i = 0;
-	if (!s1)
-		return (0);
-	while (check(set,s1[i]))
-		i++;
-	j = ft_strlen((char*)s1) - 1;
-	while (check(set, s1[j]))
-		j--;
-	len = j - i + 1;
-	str = malloc(len);
-	if (str)
-	{
-		str = ft_memccpy(str, s1,'\0',len);
-		str[len] = '\0';
-		return (str);
 	}
 	return (0);
-}*/
+}
+
+char *ft_strtrim(char const *s1, char const *set)
+{
+ 	char	*str;
+ 	size_t	i;
+ 	size_t	left;
+ 	size_t	right;
+
+ 	left = 0;
+ 	if (s1 == 0 || set == 0)
+ 		return(0);
+ 	while (s1[left] && check_set(set, s1[left]))
+ 		left++;
+ 	right = ft_strlen(s1);
+ 	while (right > left && check_set(set, s1[right - 1]))
+ 		right--;
+ 	str = malloc(right - left + 1);
+ 	if (str)
+ 	{
+ 		i = 0;
+ 		while (left < right)
+ 			str[i++] = s1[left++];
+ 		str[i] = '\0';
+ 		return (str);
+ 	}
+ 	return (0);
+}
